96e8d5f0e827d0c3b5158b5a2b78fd75725f7f49
sai8151 <saikiranreddy8151@gmail.com>
2024-10-13 14:59:16 +0530

    permissions

--- restart.sh
+++ restart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
 streamlit run admin/app.py --server.port 8501 &
 streamlit run seller/app.py --server.port 8502 &
